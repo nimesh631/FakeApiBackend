e69f5ba6ce4948e1635b77b1d3db08896b79bf1b
nimesh631 <nimeshkayastha067@gmail.com>
2024-12-26 10:20:49 +0545

    feat: set up basic Express server with CORS and body-parser middleware

---
+++ fakeApiBackend/backend/routes/product.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
